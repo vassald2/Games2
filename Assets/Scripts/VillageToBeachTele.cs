5d7970ded5c9dc5ccb59025a5ad908b6e2fd5eb9
David Vassallo <dvassallo4427@gmail.com>
2017-02-28 10:14:57 -0500

    multiple scenes added

---
+++ Assets/Scripts/VillageToBeachTele.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VillageToBeachTele : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Application.LoadLevel ("Beach"); 
+		}
+	}
+}
