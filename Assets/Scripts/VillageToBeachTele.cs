6c48c75dff15573084930b2ad773ec40a7375484
David Vassallo <dvassallo4427@gmail.com>
2017-03-07 11:51:32 -0500

    transfer between village and beach

--- Assets/Scripts/VillageToBeachTele.cs
+++ Assets/Scripts/VillageToBeachTele.cs
@@ -3,20 +3,21 @@ using System.Collections;
 
 public class VillageToBeachTele : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
 
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.gameObject.tag == "Player") {
-            AutoFade.LoadLevel("Beach", 2, 1, Color.black);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            AutoFade.LoadLevel("Beach", 1, 1, Color.black);
+            spawnPlaceHolder.fromVillage = true;
         }
-	}
+    }
+
 }
---
+++ Assets/Scripts/beachLoad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class beachLoad : MonoBehaviour {
+
+
+
+    // Use this for initialization
+    void awake() {
+        spawnPlaceHolder.fromVillage = false;
+    }
+
+	void Start () {
+        if (spawnPlaceHolder.spawned && spawnPlaceHolder.fromVillage) {
+            onLevelLoaded();
+        }
+        spawnPlaceHolder.fromVillage = false;
+        spawnPlaceHolder.spawned = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void onLevelLoaded() {
+        gameObject.transform.position = new Vector3(-30, 400, -20);
+    }
+}
